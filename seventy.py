5c00425d42f3c4221ab9dc5a29b0135dfcab566d
italo <jau.batista@dcx.ufpb.br>
2023-08-07 23:15:21 -0300

    Mini projeto completo

--- seventy.py
+++ seventy.py
@@ -1,64 +1,62 @@
 def proximoMultiplo(num):
-    if num >= 4:
-        proximo = num + (4 - (num %4))
-    else:
-        proximo = 4
-    return proximo
+    return num + (4 - (num % 4))
 
-def perdedor1():
-    print("\n\n Você Perdeu!!")
-    print("Mais sorte na proxima vez!")
+
+def perdedor1(pontos):
+    print("\n\nVocê Perdeu!!")
+    print(f"Você fez {pontos} pontos.")
+    print("Mais sorte na próxima vez!")
     exit(0)
+
 
 def verificar(xyz):
     i = 1
     while i < len(xyz):
-        if (xyz[i]-xyz[i-1])!= 1:
+        if xyz[i] - xyz[i - 1] != 1:
             return False
         i = i + 1
     return True
 
+
 def comecar1():
     xyz = []
     ultimo = 0
+    i, j = 1, 1  # Inicializando i e j
+    pontos = 0  # Inicialização dos pontos
     while True:
         print("Aperte 'F' para ter a primeira chance.")
         print("Aperte 'S' para ter a segunda chance.")
-        chance = input('>')
+        chance = input('> ')
 
         if chance == "F":
-            while True:
+            while i <= 3:
                 if ultimo == 20:
                     perdedor1()
-                else:
-                    print("\nSeu Turno.")
-                    print("\nQuantos números você deseja inserir?")
-                    inp = int(input('> '))
-                    if inp > 0 and inp <= 3:
-                        comp = 4 - inp
-                    else:
-                        print("Entrada errada. Você está desqualificado do jogo")
-                        perdedor1()
-                        i, j = 1, 1
-            print("Agora digite os valores")
-            while i <= inp:
-                    a = input('> ')
-                    a = int(a)
-                    xyz.append(a)
-                    i = i + 1
-                    ultimo = xyz[-1]    
-                    if verificar(xyz) == True:
-                        if ultimo == 21:
-                            perdedor1()
-                        else:
+                print("\nSeu Turno.")
+                print("\nQuantos números você deseja inserir?")
+                inp = int(input('> '))
+                if 0 < inp <= 3:
+                    comp = 4 - inp
+                    print("Agora digite os valores")
+                    while i <= inp:
+                        a = int(input('> '))
+                        xyz.append(a)
+                        i = i + 1
+                        ultimo = xyz[-1]
+                        if verificar(xyz) == True:
                             while j <= comp:
                                 xyz.append(ultimo + j)
                                 j = j + 1
-                                print("A ordem das entradas após a vez do computador é: ")
-                                print(xyz)
-                                ultimo = xyz[-1]
-            else:
-                print("\nVocê não inseriu números inteiros consecutivos.")
+                            print("A ordem das entradas após a vez do computador é: ")
+                            print(xyz)
+                            ultimo = xyz[-1]
+                    else:
+                        print("\nVocê não inseriu números inteiros consecutivos.")
+                        perdedor1(pontos)
+                else:
+                    print("Entrada errada. Você está desqualificado do jogo")
+                    perdedor1()
+
         elif chance == "S":
             comp = 1
             ultimo = 0
@@ -74,44 +72,43 @@ def comecar1():
                 else:
                     print("\nSeu Turno")
                     print("\nQuantos números você deseja inserir?")
-                    inp = input('> ')
-                    inp = int(inp)
+                    inp = int(input('> '))
                     i = 1
                     print("Insira seus valores")
                     while i <= inp:
                         xyz.append(int(input('> ')))
                         i = i + 1
+                        pontos += 1  # Incrementando pontos
                     ultimo = xyz[-1]
                     if verificar(xyz) == True:
                         proximo = proximoMultiplo(ultimo)
-                        comp = ultimo - ultimo
+                        comp = proximo - ultimo  # Correção no cálculo de 'comp'
                         if comp == 4:
                             comp = 3
-                        else:
-                            comp = comp
                     else:
                         print("\nVocê não inseriu números inteiros consecutivos.")
-                        perdedor1()
-            print("\nPARABENSSSSS!!!")
-            print("\nVOCÊ VENCEU!!")
-            exit(0)
-        else:
-            print("Escolha errada")
+                        perdedor1(pontos)  # Passando pontos para a função perdedor1
+                print("\nPARABENSSSSS!!!")
+                print(f"\nVOCÊ VENCEU com {pontos} pontos!!")
+                exit(0)
+            else:
+                print("Escolha errada")
+
 
 jogo = True
 while jogo == True:
     print("O jogador 2 é o computador.")
-    print("Você quer jogar o jogo de 21 números? (Sim não)")
+    print("Você quer jogar o jogo de 21 números? (Sim / Não)")
     ans = input('> ')
-    if ans == 'Yes':
+    if ans.lower() == 'sim':
         comecar1()
     else:
         print("Você quer sair do jogo? (sim / não)")
         nex = input('> ')
-        if nex == "yes":
-            print("Você esta saindo do jogo...")
+        if nex.lower() == "sim":
+            print("Você está saindo do jogo...")
             exit(0)
-        elif nex == "no":
+        elif nex.lower() == "não":
             print("Continuando...")
         else:
-            print("Esolha perigosa")
+            print("Escolha perigosa")
